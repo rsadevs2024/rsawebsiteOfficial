6ba3f0670b3dccd9428a0e80e8a3901e875e80f0
ccadevida03 <c.cadevida03@gmail.com>
2024-10-05 17:47:52 +0800

    dev1: Add Branches, Feedbacks

--- js/rsa-custom.js
+++ js/rsa-custom.js
@@ -36,3 +36,28 @@ $(document).ready(function () {
         });
     });
 });
+
+$(document).ready(function() {
+    // When a menu item is clicked, remove 'active' class from all items and add to the clicked one
+    $('.navbar-nav .nav-item a').on('click', function() {
+      $('.navbar-nav .nav-item').removeClass('active'); // Remove 'active' class from all
+      $(this).parent().addClass('active'); // Add 'active' class to the clicked item
+    });
+
+    // Optional: Highlight "Feedbacks" when scrolling to the feedback section
+    $(window).scroll(function() {
+        var bookAppointment = $('#book-appointment-section').offset().top - $(window).height() / 2;
+        if ($(window).scrollTop() >= bookAppointment) {
+          $('.navbar-nav .nav-item').removeClass('active');
+          $('.navbar-nav .nav-item a[href="#book-appointment-section"]').parent().addClass('active');
+        }
+      var sectionOffset = $('#feedback-section').offset().top - $(window).height() / 2;
+      if ($(window).scrollTop() >= sectionOffset) {
+        $('.navbar-nav .nav-item').removeClass('active');
+        $('.navbar-nav .nav-item a[href="#feedback-section"]').parent().addClass('active');
+      }
+
+     
+    });
+   
+  });
